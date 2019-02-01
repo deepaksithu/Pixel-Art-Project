9f8252aba1788e13a842863a8e9170917a6462cd
deepaksithu <47094731+deepaksithu@users.noreply.github.com>
2019-02-01 13:57:24 -0800

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,31 @@
+//set variables
+var height, width, color;
+
+//get inputs for makeGrid
+$("#sizePicker").submit(function(event) {
+  event.preventDefault();
+  height = $("#inputHeight").val();
+  width = $("#inputWidth").val();
+  makeGrid(height, width);
+});
+
+//create Grid
+function makeGrid(height, width) {
+  $("tr").remove();
+  for (var row = 1; row <= height; row++) {
+    $("#pixelCanvas").append("<tr id=table" + row + "></tr>");
+    for (var cols = 1; cols <= width; cols++) {
+      $("#table" + row).append("<td></td>");
+    }
+  }
+
+//add colors
+$("td").click(function addColor() {
+    color = $("#colorPicker").val();
+    if ($(this).attr("style")) {
+      $(this).removeAttr("style");
+    } else {
+      $(this).attr("style", "background-color:" + color);
+    }
+  });
+}
